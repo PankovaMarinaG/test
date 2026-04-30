2dd9b53fa8d8e0d7f4a524781c0b445f3aab58e8
Панькова Марина Геннадьевна <128835357+PankovaMarinaG@users.noreply.github.com>
2026-04-30 10:43:22 +0300

    Create pygame_twoball.py

---
+++ pygame_twoball.py
@@ -0,0 +1,138 @@
+Pastebin
+API
+tools
+faq
+paste
+Search...
+
+pankovamg
+FREE
+pankovamg
+ 
+SHARE
+TWEET
+pankovamg
+Pygame1
+pankovamg
+Apr 29th, 2026
+27
+0
+Never
+1
+ 1.21 KB | None |  
+       
+import pygame
+ 
+SIZE = WIDTH, HEIGHT = 400, 400
+ 
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+ 
+running = True
+ball_x, ball_y = 200, 200
+ball_x2, ball_y2 = 200, 200
+dx, dy = 5, 5
+radius = 20
+x, y = 0, 0
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+ 
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            dx = -5
+        if event.key == pygame.K_RIGHT:
+            dx = 5
+        if event.key == pygame.K_DOWN:
+            dy = 5
+        if event.key == pygame.K_UP:
+            dy = -5
+ 
+    if ball_x > WIDTH:
+        dx = -5
+    if ball_x < 0:
+        dx = 5
+ 
+    if ball_y > HEIGHT:
+        dy = -5
+    if ball_y < 50:
+        dy = 5
+ 
+    screen.fill((255, 255, 255))
+ 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+       x, dx = dx, x
+       y, dy = dy, y
+ 
+ 
+    ball_x += dx
+    ball_y += dy
+    ball_x2 -= dx
+    ball_y2 -= dy
+    pygame.draw.circle(screen, (255, 0, 0), (ball_x, ball_y), radius)
+    pygame.draw.circle(screen, (0, 255, 0), (ball_x2, ball_y2), 10)
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
+Advertisement
+RAW Paste Data 
+import pygame
+
+SIZE = WIDTH, HEIGHT = 400, 400
+
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+
+running = True
+ball_x, ball_y = 200, 200
+ball_x2, ball_y2 = 200, 200
+dx, dy = 5, 5
+radius = 20
+x, y = 0, 0
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            dx = -5
+        if event.key == pygame.K_RIGHT:
+            dx = 5
+        if event.key == pygame.K_DOWN:
+            dy = 5
+        if event.key == pygame.K_UP:
+            dy = -5
+
+    if ball_x > WIDTH:
+        dx = -5
+    if ball_x < 0:
+        dx = 5
+
+    if ball_y > HEIGHT:
+        dy = -5
+    if ball_y < 50:
+        dy = 5
+
+    screen.fill((255, 255, 255))
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+       x, dx = dx, x
+       y, dy = dy, y
+
+
+    ball_x += dx
+    ball_y += dy
+    ball_x2 -= dx
+    ball_y2 -= dy
+    pygame.draw.circle(screen, (255, 0, 0), (ball_x, ball_y), radius)
+    pygame.draw.circle(screen, (0, 255, 0), (ball_x2, ball_y2), 10)
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
+Comments
